712765a9b882620aee97f0a8c1e7a84527f847b1
orlova65 <75314128+orlova65@users.noreply.github.com>
2025-09-05 19:41:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -35,4 +35,32 @@ window.onload = () => {
       })
       .catch((error) => console.log("Что-то пошло не так: ", error));
   };
+   document.getElementById("button").onclick = () => {
+    window.YaAuthSuggest.init(
+      {
+        client_id: "c836289879e94f93bbfedb64310e2fe9",
+        response_type: "token",
+        redirect_uri: "https://oauth-master-class-tkge.vercel.app/token.html",
+      },
+      "https://oauth-master-class-tkge.vercel.app",
+      {
+        parentId: "buttonContainer",
+        view: "button",
+        buttonTheme: "light",
+        buttonSize: "xs",
+        buttonBorderRadius: 20,
+      }
+    )
+      .then(({ handler }) => handler())
+      .then(async (data) => {
+        console.log("Сообщение с токеном(от кнопки): ", data);
+
+        const result = await fetchYandexData(data.access_token);
+
+        console.log("Сообщение с ответом Яндекса(от кнопки): ", result);
+
+        authorize(result);
+      })
+      .catch((error) => console.log("Что-то пошло не так: ", error));
+  };
 };
